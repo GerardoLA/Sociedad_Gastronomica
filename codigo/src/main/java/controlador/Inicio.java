5f19121cbdb87d80a355e42e0d65308634638d2a
gerardoLA <gerardolarrinaga@gmail.com>
2025-02-05 12:07:53 +0100

    Inicio pdte doPOst

--- codigo/src/main/java/controlador/Inicio.java
+++ codigo/src/main/java/controlador/Inicio.java
@@ -34,8 +34,8 @@ public class Inicio extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		int id = Integer.parseInt(request.getParameter("id"));
+		String password = request.getParameter("password");
 	}
 
 }
